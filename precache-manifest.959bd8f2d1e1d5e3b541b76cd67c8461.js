f92e906d0e8bcb99222afa108233f6028f1716f9
Yaroslav Kaniev <y.kaniev@gmail.com>
2020-10-01 16:44:24 +0300

    Updates

---
+++ precache-manifest.959bd8f2d1e1d5e3b541b76cd67c8461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdf2bae680657ec7978acd026e9ed22",
+    "url": "/grid_resize/index.html"
+  },
+  {
+    "revision": "be3159af9d80e3cc021e",
+    "url": "/grid_resize/static/css/main.38ba1380.chunk.css"
+  },
+  {
+    "revision": "698fd528d9273f49fbf3",
+    "url": "/grid_resize/static/js/2.79a4cd13.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/grid_resize/static/js/2.79a4cd13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3159af9d80e3cc021e",
+    "url": "/grid_resize/static/js/main.fb58d694.chunk.js"
+  },
+  {
+    "revision": "80f0d5fd5ba42127d637",
+    "url": "/grid_resize/static/js/runtime-main.5d92b789.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/grid_resize/static/media/logo.5d5d9eef.svg"
+  }
+]);
